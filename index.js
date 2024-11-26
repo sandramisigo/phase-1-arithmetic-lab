1956eb015b3a7b9c2d1034c713041964110210a7
Sandra Misigo <misigosandra@gmail.com>
2024-11-26 23:12:02 +0300

    my arithmeticphase1lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply)
+
+// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random)
+
+// Write your code here
+const num3 = 15;
+const num4 = 11;
+const mod = num3 % num4;
+console.log(mod)
+
+// Write your code here
+const numbers = [8, 20, 10, 16];
+const max = Math.max(...numbers);
+console.log(max)
